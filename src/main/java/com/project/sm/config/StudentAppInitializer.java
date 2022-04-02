c434a2e7a5ccadb0bc7684ef7fc390b4387eed0e
collins <collinsboniface1000@gmail.com>
2022-04-02 17:45:00 +0300

    Created a class to config the Dispatcher Servlet which will config my servlet mappings and read my beans file or configuration file for my application

---
+++ src/main/java/com/project/sm/config/StudentAppInitializer.java
@@ -0,0 +1,24 @@
+package com.project.sm.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class StudentAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[] mappings = {"/"};
+
+        return mappings;
+    }
+
+}
