9222caf6786d09f7c279182f932215af0d3e2a99
collins <collinsboniface1000@gmail.com>
2022-04-03 19:04:58 +0300

    Added the view resolver in the configuration file which will give as the WebApplication Context

--- src/main/java/com/project/sm/config/StudentAppConfig.java
+++ src/main/java/com/project/sm/config/StudentAppConfig.java
@@ -1,13 +1,24 @@
 package com.project.sm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @Configuration
 @EnableWebMvc // This annotation will ensure all the Spring MVC features are activated
 @ComponentScan(basePackages = {"com.project.sm"})
 public class StudentAppConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
 
 }
