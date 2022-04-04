e9a3713416de662c5d24433ab107a7990d6947a3
collins <collinsboniface1000@gmail.com>
2022-04-04 12:45:51 +0300

    Created a new dependency and used the Autowired annotation to tell spring to inject the object into the dependency and used the object to use the loadStudent method which will get us the students from the database

--- src/main/java/com/project/sm/controllers/StudentController.java
+++ src/main/java/com/project/sm/controllers/StudentController.java
@@ -1,16 +1,28 @@
 package com.project.sm.controllers;
 
+import com.project.sm.DAO.StudentDAO;
+import com.project.sm.api.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
+// Presentation Layer
 @Controller
 public class StudentController {
+
+    @Autowired
+    StudentDAO studentDAO;
 
     //@GetMapping("/showStudent")
     @RequestMapping(value = "/showStudent", method = RequestMethod.GET)
     public String showStudentList() {
+
+        // Call the DAO method to get the data
+        List<Student> studentList = studentDAO.loadStudent();
 
         return "student-list";
     }
