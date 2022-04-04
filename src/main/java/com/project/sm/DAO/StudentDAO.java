5a9577ed2230df56139bad527437b725d088fb86
collins <collinsboniface1000@gmail.com>
2022-04-04 12:28:49 +0300

    Created a DAO class with one method and a POJO class for Student where the objects will be created for my Students

---
+++ src/main/java/com/project/sm/DAO/StudentDAO.java
@@ -0,0 +1,11 @@
+package com.project.sm.DAO;
+
+import com.project.sm.api.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    List<Student> loadStudent();
+
+}
---
+++ src/main/java/com/project/sm/api/Student.java
@@ -0,0 +1,43 @@
+package com.project.sm.api;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private long mobile;
+    private String country;
+
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
