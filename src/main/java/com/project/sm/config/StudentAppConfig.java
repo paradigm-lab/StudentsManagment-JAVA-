ce2f7be950eedf8712deb1764bdf01e48e2dab7a
collins <collinsboniface1000@gmail.com>
2022-04-05 14:53:10 +0300

    Created a jdbcTemplate bean and the DataSource bean and injected the dataSource object into the JdbcTemplate constructor

--- src/main/java/com/project/sm/config/StudentAppConfig.java
+++ src/main/java/com/project/sm/config/StudentAppConfig.java
@@ -3,8 +3,12 @@ package com.project.sm.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -19,6 +23,28 @@ public class StudentAppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
+
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        // Set the DB info to the dataSource object
+        dataSource.setUsername("student");
+        dataSource.setPassword("student");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/student?useSSL=false");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+
+        return dataSource;
     }
 
 }
