0858446d6665a008b82758121cdb53a6c605cb31
collins <collinsboniface1000@gmail.com>
2022-04-05 13:58:06 +0300

    Implemented the DAO interface and override the loadStudent and added the jdbcTemplate dependency in  the StudentDAOImpl class

--- src/main/java/com/project/sm/DAO/StudentDAOImpl.java
+++ src/main/java/com/project/sm/DAO/StudentDAOImpl.java
@@ -1,18 +1,35 @@
 package com.project.sm.DAO;
 
 import com.project.sm.api.Student;
+import com.project.sm.rowmapper.StudentRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+
+// DAO layer
 @Repository // Help in exception handling
 public class StudentDAOImpl implements StudentDAO{
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public List<Student> loadStudent() {
+
+        List<Student> studentList = new ArrayList<Student>();
+
+        String sql = "SELECT * FROM students";
+
         // I will write same logic to get me the studentData from the database
-        
-        return null;
+        List<Student> theListOfStudent = jdbcTemplate.query(sql, new StudentRowMapper());
+
+
+        return theListOfStudent;
     }
 
 }
